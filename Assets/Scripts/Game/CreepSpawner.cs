22ab9035416e50cf52ac8a67c2a41719889f13ce
Emanuel Tavares <emanuel.tavares@gazeus.com>
2021-09-18 19:00:54 -0300

    Add movement

---
+++ Assets/Scripts/Game/CreepSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class CreepSpawner : MonoBehaviour
+{
+    private enum CreepType { Small, Big }
+
+    [System.Serializable]
+    private class CreepTypeToPool : SerializableDictionary<CreepType, PrototypePool> { }
+
+    [SerializeField] private CreepTypeToPool _creepTypeToPool = new CreepTypeToPool();
+
+    private enum SpawnPoint { Northwest, Northeast, Southwest, Southeast }
+
+    [System.Serializable]
+    private class SpawnPointToTransform : SerializableDictionary<SpawnPoint, Transform> { }
+
+    [SerializeField] private SpawnPointToTransform _spawnPointToTransform = new SpawnPointToTransform();
+
+    private IEnumerator Start()
+    {
+        SpawnPoint[] spawnPoints = (SpawnPoint[])System.Enum.GetValues(typeof(SpawnPoint));
+
+        foreach (var pool in _creepTypeToPool.Values)
+        {
+            pool.CreatePool();
+        }
+
+        foreach (var pool in _creepTypeToPool.Values)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int index = Random.Range(0, spawnPoints.Length);
+                SpawnPoint spawnPoint = spawnPoints[index];
+                Transform spawnPointTransform = _spawnPointToTransform[spawnPoint];
+
+                pool.Spawn(spawnPointTransform.position, Quaternion.identity);
+
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PrototypePool.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrototypePool
+{
+    [SerializeField] private GameObject _prototype;
+    [SerializeField] private Transform _parent;
+    [SerializeField, Min(0)] private int _initialSize;
+
+    private readonly Stack<GameObject> _pooledInstances = new Stack<GameObject>();
+
+    public void CreatePool()
+    {
+        while (_pooledInstances.Count < _initialSize)
+        {
+            GameObject pooledInstance = Object.Instantiate(_prototype);
+            pooledInstance.transform.SetParent(_parent);
+            pooledInstance.SetActive(false);
+            _pooledInstances.Push(pooledInstance);
+        }
+    }
+
+    public void DestroyPool()
+    {
+        var e = _pooledInstances.GetEnumerator();
+        while (e.MoveNext())
+        {
+            Object.Destroy(e.Current);
+        }
+
+        _pooledInstances.Clear();
+    }
+
+    public GameObject Spawn(Vector3 position, Quaternion rotation)
+    {
+        if (_pooledInstances.Count > 0)
+        {
+            GameObject pooledInstance = _pooledInstances.Pop();
+            pooledInstance.SetActive(true);
+            pooledInstance.transform.position = position;
+            pooledInstance.transform.rotation = rotation;
+            return pooledInstance;
+        }
+        else
+        {
+            GameObject pooledInstance = Object.Instantiate(_prototype);
+            pooledInstance.SetActive(true);
+            pooledInstance.transform.position = position;
+            pooledInstance.transform.rotation = rotation;
+            return pooledInstance;
+        }
+    }
+
+    public void Despawn(GameObject go)
+    {
+        go.transform.SetParent(_parent);
+        go.SetActive(false);
+        _pooledInstances.Push(go);
+    }
+}
