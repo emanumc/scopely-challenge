01fed896c6bbc5ea3b083de8d6664b6d02f73fd0
Emanuel Tavares <emanuel.tavares@gazeus.com>
2021-09-20 16:27:57 -0300

    Add waves

--- Assets/Scripts/Game/CreepSpawner.cs
+++ Assets/Scripts/Game/CreepSpawner.cs
@@ -8,6 +8,14 @@ public enum CreepType
     Big 
 }
 
+public enum SpawnPoint 
+{ 
+    Northwest, 
+    Northeast, 
+    Southwest, 
+    Southeast 
+}
+
 public class CreepSpawner : MonoBehaviour
 {
     [System.Serializable]
@@ -15,16 +23,18 @@ public class CreepSpawner : MonoBehaviour
 
     [SerializeField] private CreepTypeToPool _creepTypeToPool = new CreepTypeToPool();
 
-    private enum SpawnPoint { Northwest, Northeast, Southwest, Southeast }
-
     [System.Serializable]
     private class SpawnPointToTransform : SerializableDictionary<SpawnPoint, Transform> { }
 
     [SerializeField] private SpawnPointToTransform _spawnPointToTransform = new SpawnPointToTransform();
 
+    [SerializeField] private Wave[] _waves = new Wave[0];
+
     public List<(CreepType, Health)> Enemies { get; private set; } = new List<(CreepType, Health)>();
 
     public bool FinishSpawning { get; private set; }
+
+    public Wave CurrentWave { get; private set; }
 
     private void Start()
     {
@@ -35,37 +45,54 @@ public class CreepSpawner : MonoBehaviour
             pool.CreatePool();
         }
 
-        StartCoroutine(CreateEnemies());
-        StartCoroutine(ManageEnemies());
+        StartCoroutine(SpawnWaves());
+        StartCoroutine(DespawnDeadEnemies());
     }
 
-    private IEnumerator CreateEnemies()
+    private IEnumerator SpawnWaves()
     {
-        SpawnPoint[] spawnPoints = (SpawnPoint[])System.Enum.GetValues(typeof(SpawnPoint));
-
-        foreach (var item in _creepTypeToPool)
+        for (int i = 0; i < _waves.Length; i++)
         {
-            for (int i = 0; i < 10; i++)
+            CurrentWave = _waves[i];
+            float maxWaitTime = float.MinValue;
+
+            foreach (var spawn in CurrentWave.Spawns)
             {
-                CreepType creepType = item.Key;
-                PrototypePool pool = item.Value;
+                SpawnPoint spawnPoint = spawn.Key;
+                Wave.CreepSpawn creepSpawn = spawn.Value;
+                StartCoroutine(SpawnCreeps(spawnPoint, creepSpawn));
 
-                int index = Random.Range(0, spawnPoints.Length);
-                SpawnPoint spawnPoint = spawnPoints[index];
-                Transform spawnPointTransform = _spawnPointToTransform[spawnPoint];
-
-                GameObject instance = pool.Spawn(spawnPointTransform.position);
-                Health health = instance.GetComponent<Health>();
-                Enemies.Add((creepType, health));
-
-                yield return new WaitForSeconds(1f);
+                float waitTime = creepSpawn.numSpawns * creepSpawn.spawnIntervalInSeconds;
+                maxWaitTime = Mathf.Max(maxWaitTime, waitTime);
             }
+
+            yield return new WaitForSeconds(maxWaitTime);
+            yield return new WaitUntil(() => Enemies.Count <= 0);
         }
 
         FinishSpawning = true;
     }
 
-    private IEnumerator ManageEnemies()
+    private IEnumerator SpawnCreeps(SpawnPoint spawnPoint, Wave.CreepSpawn creepSpawn)
+    {
+        Vector3 spawnPointPosition = _spawnPointToTransform[spawnPoint].position;
+        CreepType creepType = creepSpawn.creepType;
+        PrototypePool pool = _creepTypeToPool[creepType];
+        float spawnInterval = creepSpawn.spawnIntervalInSeconds;
+
+        for (int i = 0; i < creepSpawn.numSpawns; i++)
+        {
+            GameObject enemy = pool.Spawn(spawnPointPosition);
+            Health enemyHealth = enemy.GetComponentInParent<Health>();
+            enemyHealth.Restore(enemyHealth.MaxValue);
+
+            Enemies.Add((creepType, enemyHealth));
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private IEnumerator DespawnDeadEnemies()
     {
         while (!FinishSpawning || Enemies.Count > 0)
         {
